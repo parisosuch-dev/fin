e45317ae30c82e0281daa55c58815ff3dd064cb8
paris osuch <paris.osuch@gmail.com>
2024-08-30 21:01:43 -0700

    Add more padding to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} p-8`}>
+      <body className={`${inter.className} p-16`}>
         <Nav />
         {children}
       </body>
