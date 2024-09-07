3dacfe00e91d77c316c88fc7acfd2ff69ccfe48e
paris osuch <paris.osuch@gmail.com>
2024-09-07 16:05:36 -0700

    Configure PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,21 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Fin",
   description: "Expense and budget tracking.",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["fin", "finance", "budget", "expense"],
+  authors: [
+    {
+      name: "Paris Osuch",
+      url: "https://parisosuch.com",
+    },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "logos/logo-128.png" },
+    { rel: "icon", url: "logos/logo-128.png" },
+  ],
 };
 
 export default function RootLayout({
