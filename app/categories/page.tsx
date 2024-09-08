cc20d69400371e9c83fff5103315cb5e55d5b2b6
paris osuch <paris.osuch@gmail.com>
2024-09-08 15:24:26 -0700

    Add drawer/dialog for deleting category

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -3,9 +3,27 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardTitle } from "@/components/ui/card";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Skeleton } from "@/components/ui/skeleton";
+import useMediaQuery from "@/lib/hooks/use-media-query";
 import { getCategories } from "@/lib/supabase/category";
-
 import { createClient } from "@/lib/supabase/client";
 import { Category } from "@/lib/supabase/models";
 import { CirclePlusIcon, Trash2Icon } from "lucide-react";
@@ -15,9 +33,11 @@ import { useEffect, useState } from "react";
 
 export default function CategoriesPage() {
   const [categories, setCategories] = useState<Category[]>([]);
+  const [open, setOpen] = useState(false);
 
   const supabase = createClient();
   const router = useRouter();
+  const isDesktop = useMediaQuery({ query: "(min-width: 1024px)" });
 
   useEffect(() => {
     getCategories(supabase).then((res) => {
@@ -27,6 +47,59 @@ export default function CategoriesPage() {
       setCategories(res);
     });
   }, []);
+
+  const MobileDrawer = () => {
+    return (
+      <Drawer>
+        <DrawerTrigger asChild>
+          <Trash2Icon
+            color="gray"
+            size={18}
+            className="group-hover:scale-90 transform duration-300"
+          />
+        </DrawerTrigger>
+        <DrawerContent>
+          <div className="mx-auto w-full max-w-sm">
+            <DrawerHeader>
+              <DrawerTitle>Move Goal</DrawerTitle>
+              <DrawerDescription>
+                Set your daily activity goal.
+              </DrawerDescription>
+            </DrawerHeader>
+            <div className="p-4 pb-0"></div>
+            <DrawerFooter>
+              <Button>Submit</Button>
+              <DrawerClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </div>
+        </DrawerContent>
+      </Drawer>
+    );
+  };
+
+  const DesktopDialog = () => {
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Trash2Icon
+            color="gray"
+            size={18}
+            className="group-hover:scale-90 transform duration-300"
+          />
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you're done.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    );
+  };
 
   return (
     <div className="flex flex-1 flex-col items-center w-full mt-16">
@@ -56,18 +129,7 @@ export default function CategoriesPage() {
             <Card className="w-full p-4 space-y-2" key={category.name}>
               <div className="flex flex-row justify-between">
                 <CardTitle>{category.name}</CardTitle>
-                <div className="">
-                  <Link
-                    href={`/categories/delete?category=${category.name}`}
-                    className="group"
-                  >
-                    <Trash2Icon
-                      color="gray"
-                      size={18}
-                      className="group-hover:scale-90 transform duration-300"
-                    />
-                  </Link>
-                </div>
+                {isDesktop ? <DesktopDialog /> : <MobileDrawer />}
               </div>
               <Badge variant="outline">{category.bucket}</Badge>
               <p className="text-sm font-light">{category.description}</p>
