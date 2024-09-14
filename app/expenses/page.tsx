b95e69bdad21d53d6700836cd1a3c825cde0121b
paris osuch <paris.osuch@gmail.com>
2024-09-14 00:11:57 -0700

    add view for each transaction as a pop up, then create edit and delete icons to go from there

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -1,12 +1,25 @@
 "use client";
 
 import { Card } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { getCategories } from "@/lib/supabase/category";
 import { createClient } from "@/lib/supabase/client";
 import { getTransactionBetweenDates } from "@/lib/supabase/expense";
 import { Transaction } from "@/lib/supabase/models";
 import { useEffect, useState } from "react";
+import { Badge } from "@/components/ui/badge";
+import { CalendarIcon, PencilIcon, Trash2Icon, TrashIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function ExpensesPage() {
   const [expenses, setExpenses] = useState<Transaction[]>([]);
@@ -33,10 +46,6 @@ export default function ExpensesPage() {
     );
   }, []);
 
-  const EditPopUp = ({ expense }: { expense: Transaction }) => {};
-
-  const DeletePopUp = ({ expense }: { expense: Transaction }) => {};
-
   return (
     <div className="flex flex-1 flex-col items-center w-full mt-16">
       <div className="w-full sm:w-1/4">
@@ -56,25 +65,68 @@ export default function ExpensesPage() {
           </div>
         ) : (
           expenses.map((expense) => (
-            <Card className="w-full p-4 space-y-1" key={expense.name}>
-              <div className="flex flex-row space-x-2">
-                <p className="text-xs">{expense.date}</p>
-                <p className="text-xs">
-                  {expense.category_id
-                    ? categories[expense.category_id]
-                    : "No Category"}
-                </p>
-              </div>
-              <div className="flex flex-row w-full justify-between">
-                <div className="flex flex-row items-center">
-                  <p className="text-lg font-medium">{expense.name}</p>
-                </div>
-                <p>${expense.amount.toFixed(2)}</p>
-              </div>
-              <p className="text-sm font-light">
-                {expense.comment ? expense.comment : "No comment."}
-              </p>
-            </Card>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Card
+                  className="w-full p-4 space-y-1 hover:cursor-pointer"
+                  key={expense.name}
+                >
+                  <div className="flex flex-row space-x-2">
+                    <p className="text-xs">{expense.date}</p>
+                    <p className="text-xs">
+                      {expense.category_id
+                        ? categories[expense.category_id]
+                        : "No Category"}
+                    </p>
+                  </div>
+                  <div className="flex flex-row w-full justify-between">
+                    <div className="flex flex-row items-center">
+                      <p className="text-lg font-medium">{expense.name}</p>
+                    </div>
+                    <p>${expense.amount.toFixed(2)}</p>
+                  </div>
+                </Card>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle className="w-full flex flex-row items-center space-x-4">
+                    <p className="">{expense.name}</p>
+                    <Badge variant="secondary">
+                      {expense.category_id
+                        ? categories[expense.category_id]
+                        : "No category"}
+                    </Badge>
+                    <div className="flex flex-row space-x-2">
+                      <Link href={`/expenses/edit?id=${expense.id}`}>
+                        <PencilIcon
+                          size={24}
+                          color="gray"
+                          className="bg-gray-100 rounded-lg p-1 hover:scale-90 hover:cursor-pointer transform duration-300"
+                        />
+                      </Link>
+                      <Link href={`/expenses/delete?id=${expense.id}`}>
+                        <Trash2Icon
+                          size={24}
+                          color="gray"
+                          className="bg-gray-100 rounded-lg p-1 hover:scale-90 hover:cursor-pointer transform duration-300"
+                        />
+                      </Link>
+                    </div>
+                  </DialogTitle>
+                  <DialogDescription className="space-y-2">
+                    <div className="flex flex-row items-center space-x-2">
+                      <CalendarIcon size={16} />
+                      <p>{new Date(expense.date).toDateString()}</p>
+                    </div>
+                    <p>
+                      {expense.comment
+                        ? expense.comment
+                        : "No comment provided"}
+                    </p>
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
           ))
         )}
       </div>
